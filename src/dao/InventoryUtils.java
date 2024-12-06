c4c33ced8d67b284cdecfa0fbdcfa7ee3b1057b5
RoiRendal <23-16160@g.batstate-u.edu.ph>
2024-12-06 08:32:16 +0800

    Add files via upload

---
+++ src/dao/InventoryUtils.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author JEROME MACASAQUIT
+ */
+public class InventoryUtils {
+
+    public static String billPath = "C:\\Users\\JEROME MACASAQUIT\\Desktop\\Test";
+
+}
